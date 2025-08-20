64ccbb93fd29f1ff86126b592464aa079d9807c6
Hoyeon Jang <cliched.0-hoedown@icloud.com>
2025-08-20 21:35:45 +0900

    remove lualine icons and change lualine shape

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,15 +4,40 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function ()
   require('lualine').setup{
-    icons_enabled = false,
-    theme = 'auto',
+    options = {
+      icons_enabled = false,
+      component_separators = {
+        left = ' ',
+        right = ' '
+      },
+      section_separators = {
+        left = ' ',
+        right = ' '
+      },
+      theme = 'auto',
+    },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_a = {
+        'mode'
+      },
+      lualine_b = {
+        'branch',
+        'diff',
+        'diagnostics',
+      },
+      lualine_c = {
+        'filename'
+      },
+      lualine_x = {
+        'encoding',
+        'filetype'
+      },
+      lualine_y = {
+        'progress'
+      },
+      lualine_z = {
+        'location'
+      }
     }
   }
   end
